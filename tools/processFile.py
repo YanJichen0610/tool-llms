b8b6f82c2d34f5f82165ca93666840d8edcaf4a5
YanJichen0610 <1229092480@qq.com>
2024-06-27 10:49:54 +0900

    代码修改

--- tools/processFile.py
+++ tools/processFile.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import json
 import requests
@@ -87,30 +86,4 @@ def saveJson(file_path,data):
     - data: 需要储存的数据
     '''
     with open(file_path, 'w', encoding='utf-8') as f:
-=======
-import json
-
-
-
-def readJson(file_path,id2data=False):
-    '''
-    说明: 用于读取json文件
-    input: 
-    - id2data: 时候要转成id2data格式
-    '''
-    with open(file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    if id2data == True:
-        data = {d['id']:d for d in data}
-    return data
-
-def saveJson(file_path,data):
-    '''
-    说明: 用于储存json文件
-    input:
-    - file_path: 储存路径
-    - data: 需要储存的数据
-    '''
-    with open(file_path, 'w', encoding='utf-8') as f:
->>>>>>> f4bf1b57f1bc9ec48bf6b1f0ca8bbddba051e38a
         json.dump(data, f, ensure_ascii=False, indent=4)
